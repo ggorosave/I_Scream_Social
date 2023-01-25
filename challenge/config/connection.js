69c2c7261e555584cc50be7d0110e9d1878108ad
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-01-25 16:09:21 -0700

    Created connection in connection.js file

--- challenge/config/connection.js
+++ challenge/config/connection.js
@@ -1,3 +1,12 @@
-// 1. import mongoose
-// 2. make connection to mongoDB
-// 3. export connection
+// imports connect method and connection object from mongoose
+const { connect, connection } = require('mongoose');
+
+// creates connection to mongoDB localhost
+connect('mongodb://localhost:27017/iScreamSocialDB', {
+    // Delete? These options are outdated?
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+// exports connection
+module.exports = connection;
