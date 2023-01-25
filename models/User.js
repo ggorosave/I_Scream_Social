0997af7c47cdc966708d11f2e981a9df315da3fa
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-01-25 16:56:15 -0700

    Added userSchema and model to User.js

--- models/User.js
+++ models/User.js
@@ -4,3 +4,37 @@
 // 4. virtuals?
 // 5. create model (collection)
 // 6. export model
+
+// imports Schema and model APIs from mongoose
+const { Schema, model } = require('mongoose');
+
+// Schema to create user model
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: {
+                validator: function (v) {
+                    return /^([\w-_\.])+@([\w-]+\.)+([\w-]{2,4}])$/i.test(v);
+                },
+                message: 'Not a valid email!'
+            }
+        },
+        // screams - reference screams here
+        // friends - reference users here (self-reference)
+    }
+);
+
+// creates collection using userSchema for the model
+const User = model('user', userSchema);
+
+// exports User model
+module.exports = User;
