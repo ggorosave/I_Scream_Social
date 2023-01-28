ae1a0418c76a8616b615d82b2db0993a10a346c8
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-01-28 15:50:36 -0700

    Added route to find all users

--- controllers/screamController.js
+++ controllers/screamController.js
@@ -2,9 +2,18 @@ const { ObjectId } = require('mongoose').Types;
 const Scream = require('../models/Scream');
 
 module.exports = {
+    // UNTESTED
     getScreams(req, res) {
         Scream.find()
             .then((screams) => res.json(screams))
             .catch((err) => res.status(500).json(err));
+    },
+    createScream(req, res) {
+        Scream.create(req.body)
     }
+    // getSingleScream
+    // updateScream
+    // deleteScream
+    // addReaction
+    // removeReaction
 };
---
+++ routes/api/screamRoutes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const {
+    getScreams
+} = require('../../controllers/screamController');
+
+// /api/screams
+router.route('/').get(getScreams)
