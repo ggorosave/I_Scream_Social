df66626333945f574a96b915c269734811a78f6d
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-01-28 15:38:28 -0700

    Added routes to find a single user, update a user, delete a user, add a friend, and remove a friend

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,10 +2,24 @@ const router = require('express').Router();
 // imports controller methods
 const {
     getUsers,
-    createUser
+    createUser,
+    getSingleUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+
 } = require('../../controllers/userController');
 
 // /api/users
 router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+
 
 module.exports = router;
